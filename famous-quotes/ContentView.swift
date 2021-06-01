96bbb7b1ece6d9340977a4129b8802eeb103dc17
SwiftDom <domanik.johnson@icloud.com>
2021-05-31 22:12:33 -0400

    Initial Commit

---
+++ famous-quotes/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  famous-quotes
+//
+//  Created by Domanik Johnson on 5/31/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ famous-quotes/famous_quotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  famous_quotesApp.swift
+//  famous-quotes
+//
+//  Created by Domanik Johnson on 5/31/21.
+//
+
+import SwiftUI
+
+@main
+struct famous_quotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
